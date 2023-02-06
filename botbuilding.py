adf5bf61255d0a1e080991c98cc2d2e747fcf3c0
Alfred <56558930+a1f23d@users.noreply.github.com>
2023-02-06 07:52:15 -0500

    Add files via upload

---
+++ botbuilding.py
@@ -0,0 +1,43 @@
+import discord
+import random
+
+
+TOKEN = 'MTA2MzEwMDYwMDI5Nzk5NjQyOQ.GXngDb.gjxc1YHrZ3MX6BKL_zPhSwa5fi0_iBLga1nVOQ'
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents = intents)
+
+@client.event
+async def on_ready():
+    print('logged in as {0.user}'.format(client))
+
+@client.event
+    #take message as parameter
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_message} ({channel})') #logging everything on server
+
+
+    if message.author == client.user:
+        return
+
+    if message.channel.name == 'bot-testing':
+        if user_message.lower() == 'hello':
+            await message.channel.send(f'Hi {username}!')
+            return
+    elif user_message.lower() == 'bye':
+        await message.channel.send(f'Until we meet again, {username}!')
+        return
+    elif user_message.lower() == "!random":
+        response = f'Your random number is: {random.randrange(10000)}'
+        await message.channel.send(response)
+        return
+
+    if user_message.lower() == '!anywhere':
+        await message.channel.send('Usage here is ill advised')
+        return
+
+
+client.run(TOKEN)
